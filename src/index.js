b6fb0c16a8a147fc08172dec576fa47d126a49ab
Arturo Monroy <ingatorrez@gmail.com>
2022-03-29 20:30:22 -0600

    Updated to React 18.0.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot }  from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { LangProvider } from "./Context/LangContext";
 import App from "./App";
 
+//React 18.0.0
+createRoot(document.getElementById("root")).render(
+  <LangProvider>
+    <App />
+  </LangProvider>
+);
+/* React 17.0.0
 ReactDOM.render(
   <LangProvider>
     <React.StrictMode>
@@ -12,7 +19,7 @@ ReactDOM.render(
     </React.StrictMode>
   </LangProvider>,
   document.getElementById("root")
-);
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
